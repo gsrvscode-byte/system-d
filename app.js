957cd00b18946f3876e71da794f707920cadbe3b
gsrvscode-byte <gsrvscode@gmail.com>
2026-08-12 12:27:12 +0530

    Added dev

--- app.js
+++ app.js
@@ -46,6 +46,10 @@ app.get('/test_qa', (req, res) => {
   res.json({ success: true, message: 'Welcome to the QA route' });
 });
 
+app.get('/test_dev', (req, res) => {
+  res.json({ success: true, message: 'Welcome to the QA route' });
+});
+
 // Health check
 app.get('/health', (req, res) => res.json({ success: true, message: 'Server is running' }));
 
